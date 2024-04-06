e8f51f26a7a81742ff66435b53b41ec9584b5c1f
stan-alexandru <stan.alexandru26@gmail.com>
2024-04-07 02:10:36 +0300

    feat:add footer

---
+++ src/data/index.ts
@@ -0,0 +1,39 @@
+export const HEADER = [
+	{
+		title: 'About us',
+		href: '#',
+	},
+	{
+		title: 'Services',
+		href: '#',
+	},
+	{
+		title: 'Use Cases',
+		href: '#',
+	},
+	{
+		title: 'Pricing',
+		href: '#',
+	},
+	{
+		title: 'Blog',
+		href: '#',
+	},
+];
+export const SOCIAL = [
+	{
+		title: 'LinkedIn',
+		href: '#',
+		path: '/public/assets/icon/linkedin.svg',
+	},
+	{
+		title: 'Facebook',
+		href: '#',
+		path: '/public/assets/icon/facebook.svg',
+	},
+	{
+		title: 'Twitter',
+		href: '#',
+		path: '/public/assets/icon/twitter.svg',
+	},
+];
